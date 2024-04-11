bb068881eca5928f118ecd8a2c0fad9b95786b0f
Nilles3D <98187695+Nilles3D@users.noreply.github.com>
2024-04-11 18:06:16 -0500

    add1
    xbot function 1 of many

--- gnss.py
+++ gnss.py
@@ -10,6 +10,7 @@
 #  
 from ublox_gps import UbloxGps
 from radioSerial import serLocalGPIO
+import struct
 
 
 #Serial inic
@@ -60,24 +61,58 @@ def getPosition():
     
     return myPosition
     
-def sendWT(wtL,wtR):
+
+
+def sendWT(wtL,wtR,timestamp):
     #compose message for sending integer Left and Right Wheel Ticks
     # where 1 WT  = 5 cm of linear travel
     #return nothing
-    
+    #credit: https://github.com/ClemensElflein/xbot_driver_gps/blob/main/src/ublox_gps_interface.cpp#L128
+
+    #interpret signed inputs
     wtL=int(wtL)
     wtR=int(wtR)
+    #scale
+    bit22L=abs(wtL)
+    bit22R=abs(wtR)
+    #direction
+    fwdL=(wtL>0)
+    fwdR=(wtR<0)
+    endL=0 if fwdL else 1
+    endR=0 if fwdR else 1
+
     
-    endL=0 if wtL>0 else 1
-    endR=0 if wtR>0 else 1
+    #Compose and send message
+    frame = bytearray(8 + 2 * 4 + 8)
     
-    bit22L=wtL
-    bit22R=wtR
-    
-    #compose and send message
-    
+    # Set the message class and ID
+    frame[2] = 0x10
+    frame[3] = 0x02
+
+    #Begin payload construction
+    payload = struct.unpack_from('IIII', frame, 6)
+    payload[0] = timestamp
+    # flags etc, it's all 0
+    payload[1] = 0
+
+    data_left = bit22L & 0x7FFFFF
+    if fwdL:
+        data_left |= 1 << 23
+    data_left |= 8 << 24
+    payload[2] = data_left
+
+    data_right = bti22R & 0x7FFFFF
+    if fwdR:
+        data_right |= 1 << 23
+    data_right |= 9 << 24
+    payload[3] = data_right
+
+    #Send payload
+    struct.pack_into('IIII', frame, 6, *payload)
+    send_packet(frame, len(frame))
     
     return
+
 
 if __name__ == '__main__':
     print(iniciar())
